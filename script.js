3952826506c69b39f6e61956c3648a6b187f61e1
Mouzone <sunnyliu010@gmail.com>
2024-08-02 12:31:59 -0400

    Refactor to use async and await

--- script.js
+++ script.js
@@ -1,34 +1,29 @@
-function updateGif(searchTerm) {
+async function updateGif(searchTerm) {
     const apiKey = "ysNMO89bq3vh9HQtkwfconzJD5RadqiI"
     const linkPrefix = "https://api.giphy.com/v1/gifs/translate"
     const link = `${linkPrefix}?api_key=${apiKey}&s=${searchTerm}`
 
-    fetch(link, { mode: 'cors' })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`Error: ${response.status}`)
-            }
-            return response.json()
-        })
-        .then(data => {
-            if (data.data.length === 0) {
-                throw new Error("GIF not found")
-            }
+    try {
+        const response = await fetch(link, { mode: 'cors' })
+        const gifData = await response.json()
+        if (gifData.data.length === 0) {
+            throw new Error("GIF not found")
+        } else {
             const img = document.querySelector("img")
-            img.src = data.data.images.original.url
-        })
-        .catch(error => {
-            console.error("Error fetching GIF:", error)
-        })
+            img.src = gifData.data.images.original.url
+        }
+    } catch(error) {
+        console.error("Error fetching GIF:", error)
+    }
 }
 
 const form = document.querySelector("form")
 const input = document.querySelector("input")
 
-form.addEventListener("submit", event => {
+form.addEventListener("submit", async event => {
     event.preventDefault()
     const searchTerm = input.value.trim() || "cats"
-    updateGif(searchTerm)
+    await updateGif(searchTerm)
     input.value = ""
 })
 
